9daacad4213890ad251755a0e8644137267f7097
suzukiken <suzukiken@gmail.com>
2021-12-24 12:28:53 +0000

    Initial commit

---
+++ test/cdk-appsync-stepfunctions.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CdkAppsyncStepfunctions from '../lib/cdk-appsync-stepfunctions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-appsync-stepfunctions-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAppsyncStepfunctions.CdkAppsyncStepfunctionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
