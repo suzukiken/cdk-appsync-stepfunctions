f9d5e85ddb711ab97cc6582fef061aba9ff06417
suzukiken <suzukiken@gmail.com>
2021-12-24 17:03:10 +0000

    first

--- lib/cdk-appsync-stepfunctions-stack.ts
+++ lib/cdk-appsync-stepfunctions-stack.ts
@@ -1,15 +1,103 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as appsync from "@aws-cdk/aws-appsync";
+import * as iam from "@aws-cdk/aws-iam";
+import * as sfn from "@aws-cdk/aws-stepfunctions";
 
 export class CdkAppsyncStepfunctionsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
+    
+    // state machine
+    
+    const task = new sfn.Wait(this, "task", {
+      time: sfn.WaitTime.duration(cdk.Duration.seconds(5)),
+    })
+    
+    const definition = task
 
-    // The code that defines your stack goes here
+    const state_machine = new sfn.StateMachine(this, "state_machine", {
+      definition,
+      timeout: cdk.Duration.seconds(10),
+    })
+    
+    // iam role
+    
+    var role = new iam.Role(this, "role", {
+      assumedBy: new iam.ServicePrincipal('appsync.amazonaws.com')
+    })
+    
+    const api_policy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "states:StartExecution"
+      ],
+      resources: [ 
+        state_machine.stateMachineArn
+      ]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkAppsyncStepfunctionsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    role.attachInlinePolicy(new iam.Policy(this, 'AuthPolicy', {
+      statements: [
+        api_policy
+      ]
+    }))
+    
+    // api
+    
+    const schema = new appsync.Schema({
+      filePath: "graphql/schema.graphql",
+    })
+    
+    const api = new appsync.GraphqlApi(this, "Api", {
+      name: id + "Api",
+      logConfig: {
+        fieldLogLevel: appsync.FieldLogLevel.ALL,
+      },
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.IAM,
+        }
+      },
+      schema: schema,
+    })
+    
+    const httpds = api.addHttpDataSource(
+      'HttpDataSource',
+      'https://states.ap-northeast-1.amazonaws.com/',
+      {
+        authorizationConfig: {
+          signingRegion: 'ap-northeast-1',
+          signingServiceName: 'states',
+        },
+      },
+    )
+    
+    state_machine.grantStartExecution(httpds)
+    
+    httpds.createResolver({
+      typeName: 'Query',
+      fieldName: 'run',
+      requestMappingTemplate: appsync.MappingTemplate.fromString(
+        `{
+          "version": "2018-05-29",
+          "method": "POST",
+          "resourcePath": "/",
+          "params": {
+            "headers": {
+              "content-type": "application/x-amz-json-1.0",
+              "x-amz-target":"AWSStepFunctions.StartExecution"
+            },
+            "body": {
+              "stateMachineArn": "` + state_machine.stateMachineArn + `"
+            }
+          }
+        }`  
+      ),
+      responseMappingTemplate: appsync.MappingTemplate.fromString(`$util.toJson($ctx.result)`)
+    })
+    
   }
 }
+
+
+
